2b93efbe3d23fe807e1980dd27a1b1b6a577c6fc
Noah Worley <noah.worley921@gmail.com>
2019-04-01 15:07:27 -0400

    Update GPIOClassTest2.cpp
    updated pin 4 to pin 6

--- GPIOClassTest2.cpp
+++ GPIOClassTest2.cpp
@@ -7,13 +7,13 @@ int main (void)
 
     string inputstate;
     int i = 0;
-    GPIOClass* gpio4 = new GPIOClass("4"); //create new GPIO object to be attached to  GPIO4
-    GPIOClass* gpio5 = new GPIOClass("5"); //create new GPIO object to be attached to  GPIO17
+    GPIOClass* gpio6 = new GPIOClass("6"); //create new GPIO object to be attached to  GPIO6
+    GPIOClass* gpio5 = new GPIOClass("5"); //create new GPIO object to be attached to  GPIO5
 
     cout << " GPIO pins exported" << endl;
 
     gpio5->setdir_gpio("out"); //GPIO5 set to output
-    gpio4->setdir_gpio("out"); // GPIO4 set to input
+    gpio6->setdir_gpio("out"); // GPIO6 set to input
 
     cout << " Set GPIO pin directions" << endl;
 
@@ -22,9 +22,9 @@ int main (void)
         usleep(500000);
         usleep(20000);
         cout << "input pin state is definitely \"Pressed\". Turning LED ON" <<endl;
-        gpio4->setval_gpio("1"); // turn LED ON
+        gpio6->setval_gpio("1"); // turn LED ON
         usleep(2000000);
-        gpio4->setval_gpio("0");
+        gpio6->setval_gpio("0");
         gpio5->setval_gpio("1");
         usleep(2000000);
         gpio5->setval_gpio("0");
@@ -32,9 +32,9 @@ int main (void)
     }
 
     cout << "Releasing heap memory and exiting....." << endl;
-    delete gpio4;
+    delete gpio6;
     delete gpio5;
-    gpio4 = NULL;
+    gpio6 = NULL;
     gpio5 = NULL;
 
     return 0;
