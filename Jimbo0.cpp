bb4744756db793a92a7c51b0321dfb8626c7ff3d
Noah Worley <noah.worley921@gmail.com>
2020-02-25 19:00:23 -0500

    New Jimbo Class
    Created Jimbo0 class which should have a fixed clock function, though I am not sure if it works correctly yet.
    
    I replaced where we previous updated time values with a different method of tracking times, but not their placements.
         Either it:
              -Fixes everything
              -Doesnt work at all
              -Works, but still has timekeeping issues present in old method

---
+++ Jimbo0.cpp
@@ -0,0 +1,307 @@
+
+#include "Jimbo0.h"
+
+
+
+using namespace std;
+//##############################################################################
+//class definitions
+//##############################################################################
+//averages an array of points
+double Jimbo::average(double *points, int numPnts){
+  double mean;
+  for(int i = 0; i<numPnts; i++){
+    mean += points[i]/(float)numPnts;
+  }
+
+  gettimeofday(&currT, NULL);
+  dt = (currT.tv_sec-prevT.tv_sec)
+		+(currT.tv_usec-prevT.tv_usec)/1000000.0;
+  prevT=currT;
+  return mean;
+}
+
+//unsure what to place here, so currently is used as a placeholder
+void Jimbo::beep(int freqi, double length, int freqf, bool rising){ //beepu
+  if(rising){
+    int steps = freqf-freqi;
+    int i = freqi;
+
+    while(i<freqf){
+      softToneWrite(beepPin, i);
+      usleep((int)(length/steps*1000000));
+      i++;
+    }
+  }
+  else if (length>0){
+    softToneWrite(beepPin, freqi);
+    usleep((int)(length*1000000));
+    softToneWrite(beepPin, 0);
+  }
+  else{
+    softToneWrite(beepPin, freqi);
+  }
+}
+
+//takes an average of heights for the new current height as a parameter, and updates the
+// current height and previous heights in measure. Similarly, checks to see if
+// the max height has increased.
+void Jimbo::setH(){
+  preH = curH;
+  curH = average(avgArray, window);
+  altimeterData << curH << ", " << getTotTime() << endl;
+  //cout << curH << endl;
+  //clearScreen();
+
+  if(maxH<curH){
+    maxH = curH;
+  }
+}
+
+//uses the change in height over the change in time to compute the current velocity
+// whilst also updating the previous velocity. Similarly, checks to see if the
+// max velocity has increased.
+void Jimbo::setV(){
+  preV = curV;
+  curV = (curH-preH)/((float)dt);
+
+  if(maxV<curV){
+    maxV = curV;
+  }
+}
+
+//uses the change in velocity over the change in time to compute the current
+// acceleration whilst also updating the previous acceleration. Similarly, checks
+// to see if the max acceleration has increased.
+void Jimbo::setA(){
+  preA = curA;
+  curA = (curV-preV)/((float)dt);
+
+  if(maxA<curA){
+    maxA = curA;
+  }
+}
+
+//appends an input to a circular array, avgArray
+void Jimbo::append(double newInput){
+  gettimeofday(&currT, NULL);
+  dt = (currT.tv_sec-prevT.tv_sec)
+		+(currT.tv_usec-prevT.tv_usec)/1000000.0;
+  prevT=currT;
+  avgArray[counter] = newInput;
+  counter++;
+  if(!(counter < window)){
+     counter = 0;
+  }
+}
+
+//fires drogue charge
+void Jimbo::fireDrogue(){
+  //turn pin attached to drogue charge to HIGH
+  gettimeofday(&currT, NULL);
+  float drogueT = dt = (currT.tv_sec-initT.tv_sec)
+		+(currT.tv_usec-initT.tv_usec)/1000000.0;
+  prevT=currT;
+  wikiHow << "Drogue fired at time: " << (int)(((double)drogueT/CLOCKS_PER_SEC)/60) << " minute(s) ";
+  wikiHow << ((int)((double)drogueT/CLOCKS_PER_SEC)%60) << " second(s)" << endl;
+  wikiHow << "Drogue fired at a height of: " << curH-initH << "m;" << endl;
+  drogueFired = 1;
+  digitalWrite(droguePin, HIGH);
+}
+
+//fire main charge
+void Jimbo::fireMain(){
+  //turn pin attached to main charge to HIGH
+  gettimeofday(&currT, NULL);
+  float mainT = dt = (currT.tv_sec-initT.tv_sec)
+		+(currT.tv_usec-initT.tv_usec)/1000000.0;
+  prevT=currT;
+  wikiHow << "Main fired at time: " << (int)(((double)mainT/CLOCKS_PER_SEC)/60) << " minute(s) ";
+  wikiHow << ((int)((double)mainT/CLOCKS_PER_SEC)%60) << " second(s)" << endl;
+  wikiHow << "Main fired at a height of: " << curH-initH << "m;" << endl;
+  mainFired = 1;
+  digitalWrite(mainPin, HIGH);
+}
+
+int Jimbo::endFlight(){
+  gettimeofday(&currT, NULL);
+  float totalT = dt = (currT.tv_sec-initT.tv_sec)
+		+(currT.tv_usec-initT.tv_usec)/1000000.0;
+  prevT=currT;
+  wikiHow << "Total time elapsed: " << (int)(getTotTime()/60) << " minute(s)";
+  wikiHow << ((int)getTotTime()%60) << " second(s);" << endl;
+  wikiHow << "Max height achieved: " << maxH << "m (that's a lot!);" << endl;
+  wikiHow << "Max velocity achieved: " << maxV << "m/s (woah, slow down there, Speed Racer);" << endl;
+  wikiHow << "Max acceleration achieved: " << maxA << "m/s/s (I think my neck hurts);" << endl;
+  wikiHow.close();
+
+  altimeterData.close();
+
+
+  landed = 1;
+
+  return 0;
+}
+
+void Jimbo::altimeterGather(){
+  config[0] = 0x26;
+	config[1] = 0xB9;
+	write(file, config, 2);
+	// Select data configuration register(0x13)
+	// Data ready event enabled for altitude, pressure, temperature(0x07)
+	config[0] = 0x13;
+	config[1] = 0x07;
+	write(file, config, 2);
+	// Select control register(0x26)
+	// Active mode, OSR = 128, altimeter mode(0xB9)
+	config[0] = 0x26;
+	config[1] = 0xB9;
+	write(file, config, 2);
+	//usleep(10000);
+        //totT = totT+0.01;
+
+
+	// Read 6 bytes of data from address 0x00(00)
+	// status, tHeight msb1, tHeight msb, tHeight lsb, temp msb, temp lsb
+	reg[1] = {0x00};
+	write(file, reg, 1);
+	data[6] = {0};
+	read(file, data, 6);
+
+	// Convert the data
+	tHeight = ((data[1] * 65536) + (data[2] * 256 + (data[3] & 0xF0)) / 16);
+	temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16;
+	altitude = tHeight / 16.0;
+	cTemp = (temp / 16.0);
+	fTemp = cTemp * 1.8 + 32;
+
+	// Select control register(0x26)
+	// Active mode, OSR = 128, barometer mode(0x39)
+	config[0] = 0x26;
+	config[1] = 0x39;
+	write(file, config, 2);
+	//usleep(10000);
+        //totT = totT+0.01;
+
+	// Read 4 bytes of data from register(0x00)
+	// status, pres msb1, pres msb, pres lsb
+	reg[0] = 0x00;
+	write(file, reg, 1);
+	read(file, data, 4);
+
+	// Convert the data to 20-bits
+	pres = ((data[1] * 65536) + (data[2] * 256 + (data[3] & 0xF0))) / 16;
+	pressure = (pres / 4.0) / 1000.0;
+
+        // Test for corrupted data
+        if(altitude > 10000)
+           altitude = prevAlt;
+        else
+           prevAlt = altitude;
+}
+
+//returns the elapsed time
+double Jimbo::getTotTime(){
+
+  gettimeofday(&currT, NULL);
+  float totT = dt = (currT.tv_sec-initT.tv_sec)
+		+(currT.tv_usec-initT.tv_usec)/1000000.0;
+  prevT=currT;
+  return(totT)
+}
+
+
+//appends a value to avgArray and updates H, V, and A;
+void Jimbo::updateAll(){
+
+  altimeterGather();
+  append(altitude);
+  setH();
+  setV();
+  setA();
+
+  if(((int)(100*(double)(clock()-totalT)/CLOCKS_PER_SEC))%200==0){
+    beep(1500);
+  }
+  if(((int)(100*(double)(clock()-totalT)/CLOCKS_PER_SEC))%225==0){
+    beep(0);
+  }
+
+
+  if((curV<0) && ((curH-initH)/(maxH-initH) <= drogueLRatio)){
+    //fireDrogue(); //removed for testing purposes
+  }
+
+  if((drogueFired) && ((curH-initH) <= mainLHFt)){
+    //fireMain(); //removed for testing purposes
+  }
+
+  if((curH<=1.01*initH) && drogueFired && mainFired && (curV <= 0.1 && curV >= -0.1) && (curA <= 0.1 && curA >= -0.1)){
+    //endFlight();
+  }
+}
+
+void Jimbo::clearScreen(){
+  cout << flush;
+  system("clear");
+}
+
+// currently empty (possibly going to be parameterized). Will initialize height.
+// velocity, and acceleration.
+Jimbo::Jimbo(){
+  gettimeofday(&currT, NULL);
+  float totalT = dt = (currT.tv_sec-initT.tv_sec)
+		+(currT.tv_usec-initT.tv_usec)/1000000.0;
+  prevT=currT;
+  wikiHow.open("wikiHow.txt");
+  altimeterData.open("altimeterData.txt");
+  //droguePin->export_gpio(); // not needed with GPIOClass_v2
+  //mainPin->export_gpio(); // not needed with GPIOClass_v2
+  cout << "I'm here!" << endl;
+  //droguePin->setdir_gpio("out"); // for testing purposes, we are taking this out, so we dont need to reboot
+  //mainPin->setdir_gpio("out");
+  cout << "Now I'm here!" << endl;
+
+  file = open(bus, O_RDWR);
+  ioctl(file, I2C_SLAVE, 0x60);
+
+  wiringPiSetupGpio();
+
+  pinMode(droguePin, OUTPUT);
+  pinMode(mainPin, OUTPUT);
+  pinMode(beepPin, OUTPUT);
+
+  softToneCreate(beepPin);
+
+
+  for(int i = 0; i<window; i++){
+    altimeterGather();
+    append(altitude);
+  }
+  setH();
+
+  initH = curH;
+  gettimeofday(&initT, NULL);
+  currT=initT
+  prevT=currT;
+  wikiHow << "Rocket starting at a height of: " << initH << "m. All proceeding heights";
+  wikiHow << " will be adjusted accordingly.;" << endl;
+
+  altimeterGather();
+  append(altitude);
+  setH();
+  setV();
+  altimeterGather();
+  append(altitude);
+  setH();
+  setV();
+  setA();
+
+  for(int i = 0; i<4; i++){
+      beep(440, 0.5, 540, true);
+      beep(0);
+      usleep(500000);
+  }
+}
+
+Jimbo::~Jimbo(){}
---
+++ Jimbo0.h
@@ -0,0 +1,144 @@
+
+//##############################################################################
+//inclusions, constants and namespace
+//##############################################################################
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include<sys/time.h>
+#include<time.h>
+#include <math.h>
+#include <fstream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <wiringPi.h>
+#include <softTone.h>
+
+#define droguePin 5
+#define mainPin 6
+#define beepPin 18
+
+const double GRAVITY = -9.8;
+const double METER_TO_FEET = 3.28084;
+const double FEET_TO_METER = 1/METER_TO_FEET;
+
+using namespace std;
+
+//##############################################################################
+//classes
+//##############################################################################
+//class for 'altimeter' variable storage
+class Jimbo{
+  private:
+
+  // I2C stuff
+  int file;
+  char *bus = "/dev/i2c-1";
+
+  char config[2] = {0};
+
+
+
+  char reg[1] = {0x00};
+	char data[6] = {0};
+
+  int tHeight, temp, pres;
+  float altitude, cTemp, fTemp, pressure;
+
+
+  //output file stream
+  ofstream wikiHow;
+  ofstream altimeterData;
+
+  //Position, Velocity, and Acceleration values
+  double preH, curH, maxH = 0;
+  double preV, curV, maxV = 0;
+  double preA, curA, maxA = 0;
+  double prevAlt = 0;
+
+  //Initial height
+  double initH;
+
+  //height to fire main charge
+  double mainLHFt = 500;
+  double mainLHM = FEET_TO_METER*mainLHFt;
+
+  //max height ratio to fire drogue charge
+  double drogueLRatio = 0.9;
+
+  //boolean to determine if the drogue 'chute has fired.
+  bool drogueFired = 0;
+  bool mainFired = 0;
+
+
+  //internal counter
+  int counter = 0;
+
+  //elapsed time
+  double totT=0;
+  double dt=0;
+  struct timeval initT, prevT, currT;
+
+  //second interval between arming beeps
+  const double byap = 1;
+
+  //averaging window
+  const static int window = 100;
+
+  //averaging array
+  double avgArray[window] = {0};
+
+  //GPIO pins
+
+
+
+
+  //averages an array of points
+  double average(double *points, int numPnts);
+
+  //arming beeps
+  void beep(int freqi, double length=0.0, int freqf=0, bool rising = false);
+
+  //updates the previous height, vel., accl., and updates their respective values
+  // based on average heights and the change in height and velocity respectively
+  void setH();
+  void setV();
+  void setA();
+
+  //appends a value, preferably the altimeter reading, to avgArray
+  void append(double);
+
+  //four is drogue; five is main
+  void fireDrogue();
+  void fireMain();
+
+  //int endFlight();
+
+  void altimeterGather();
+
+  void clearScreen();
+
+
+  public:
+
+  //test
+  int endFlight();
+
+  //returns elapsed time
+  double getTotTime();
+
+  //boolean to determine if the rocket has "landed"
+  bool landed = 0;
+
+  //appends a value to avgArray and updates H, V, and A
+  void updateAll();
+
+  //class constructor and destructor
+  Jimbo();
+  ~Jimbo();
+};
